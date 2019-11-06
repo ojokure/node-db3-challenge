ae6b50ded294242e16b6d5ab100c124294e72269
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-06 15:09:02 +0000

    created a findById helper

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,16 @@
 const db = require("../data/dbConfig");
 
 module.exports = {
-  find
+  find,
+  findById
 };
 
 function find() {
   return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
 }
